5b4dc2fdfdcfec6ebd437404d4129c8b2aa6febd
José Luis Larenas <88337732+JoseLarenas@users.noreply.github.com>
2021-09-08 11:50:53 -0300

    Update secciones.py

--- secciones.py
+++ secciones.py
@@ -3,36 +3,23 @@ from constantes import g_, ρ_acero
  
 class Circular(object):
     """define una seccion Circular"""
-
     def __init__(self, D, Dint):
         super(Circular, self).__init__()
-        """Implementar"""	
+        self.D = D
+        self.Dint = Dint
 
     def area(self):
-        
-        """Implementar"""	
-        
-        return 0
+        """Implementar"""
+        A = pi*(self.D**2-self.Dint**2)/4
+        return(A)
 
     def peso(self):
-
-        
-        """Implementar"""	
-        
-        return 0
+        peso = g_*ρ_acero*self.area()
+        return(peso)
 
     def inercia_xx(self):
-
-        
-        """Implementar"""	
-        
-        return 0
+        Ixx = pi*(self.D**4-self.Dint**4)/4
+        return(Ixx)
 
     def inercia_yy(self):
-
-        
-        """Implementar"""	
-        
-        return 0
-
-
+        return(self.inercia_xx())
