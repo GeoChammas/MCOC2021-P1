5b168e8d4e4c65dd056dd30a295888a4b7ccedb0
George Chammas <88348487+GeoChammas@users.noreply.github.com>
2021-09-09 16:50:32 -0300

    Add files via upload

--- reticulado.py
+++ reticulado.py
@@ -1,131 +1,82 @@
 import numpy as np
-
+from scipy.linalg import solve
 
 class Reticulado(object):
-    """Define un reticulado"""
-    __NNodosInit__ = 100
-
-    #constructor
-    def __init__(self):
-        super(Reticulado, self).__init__()
-        
-        print("Constructor de Reticulado")
-        
-        self.xyz = np.zeros((Reticulado.__NNodosInit__,3), dtype=np.double)
+    _NNodosInit_ = 100
+    def _init_(self):
+        super(Reticulado, self)._init_()
+        self.xyz = np.zeros((Reticulado._NNodosInit_,3), dtype=np.double)
         self.Nnodos = 0
         self.barras = []
         self.cargas = {}
         self.restricciones = {}
-        """Implementar"""	
-        
-
 
     def agregar_nodo(self, x, y, z=0):
-        
-        """Implementar"""	
-
-        print(f"Quiero agregar un nodo en ({x} {y} {z})")
-        numero_de_nodo_actual = self.Nnodos
-
-        self.xyz[numero_de_nodo_actual,:] = [x, y, z]
-
+        self.xyz.resize((self.Nnodos+1, 3))
+        self.xyz[self.Nnodos,:] = [x,y,z]
         self.Nnodos += 1
-        
-        return 0
+        return()
 
     def agregar_barra(self, barra):
-        
-        self.barras.append(barra)        
-        
-        return 0
+        self.barras.append(barra)
+        return()
 
     def obtener_coordenada_nodal(self, n):
-        
-        """Implementar"""	
-        
-        return 0
+        posicion = n
+        coordenadas = self.xyz[n]
+        return(coordenadas)
 
     def calcular_peso_total(self):
-        
-        """Implementar"""	
-        
-        return 0
+        lista_barras = self.barras
+        peso_total = 0
+        for barra in lista_barras:
+            peso_total += barra.calcular_peso(self)
+        return(peso_total)
 
     def obtener_nodos(self):
-        
-        return self.xyz
+        xy = self.xyz
+        return(xy)
 
     def obtener_barras(self):
-        
-        return self.barras
-
-
+        lista_barras = self.barras
+        return(lista_barras)
 
     def agregar_restriccion(self, nodo, gdl, valor=0.0):
-        
-        """Implementar"""	
-        
-        return 0
+        #Falta implementar	
+        return(0)
 
     def agregar_fuerza(self, nodo, gdl, valor):
-        
-        """Implementar"""	
-        
-        return 0
+        #Falta implementar	
+        return(0)
 
-
-    def ensamblar_sistema(self, factor_peso_propio=0.):
-        
-        """Implementar"""	
-        
-        return 0
-
-
+    def ensamblar_sistema(self):
+        #Falta implementar	
+        return(0)
 
     def resolver_sistema(self):
-        
-        """Implementar"""	
-        
-        return 0
+        #Falta implementar	
+        return(0)
 
     def obtener_desplazamiento_nodal(self, n):
-        
-        """Implementar"""	
-        
-        return 0
-
+        #Falta implementar	
+        return(0)
 
     def obtener_fuerzas(self):
-        
-        """Implementar"""	
-        
-        return 0
-
+        #Falta implementar	
+        return(0)
 
     def obtener_factores_de_utilizacion(self, f):
-        
-        """Implementar"""	
-        
-        return 0
+        #Falta implementar	
+        return(0)
 
     def rediseñar(self, Fu, ϕ=0.9):
-        
-        """Implementar"""	
-        
-        return 0
-
-
+        #Falta implementar	
+        return(0)
 
     def chequear_diseño(self, Fu, ϕ=0.9):
-        
-        """Implementar"""	
-        
-        return 0
-
-
-
-
-
-
+        #Falta implementar	
+        return(0)
 
     def __str__(self):
+     
+        return "Soy un reticulado :)"
