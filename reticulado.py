9e358c1f440dbe8d8b3918bdc5bce7ceb1beb859
George Chammas <88348487+GeoChammas@users.noreply.github.com>
2021-09-22 00:22:54 -0300

    Add files via upload

--- reticulado.py
+++ reticulado.py
@@ -7,19 +7,21 @@ class Reticulado(object):
         super(Reticulado, self).__init__()
         self.xyz = np.zeros((Reticulado.__NNodosInit__,3), dtype=np.double)
         self.Nnodos = 0
+        self.Ndimensiones = 3
         self.barras = []
         self.cargas = {}
         self.restricciones = {}
+        self.fuerzas = []
 
     def agregar_nodo(self, x, y, z=0):
         self.xyz.resize((self.Nnodos+1, 3))
         self.xyz[self.Nnodos,:] = [x,y,z]
         self.Nnodos += 1
-        return()
-
+        if z != 0:
+            self.Ndimensiones = 3
+            
     def agregar_barra(self, barra):
         self.barras.append(barra)
-        return()
 
     def obtener_coordenada_nodal(self, n):
         posicion = n
@@ -42,28 +44,59 @@ class Reticulado(object):
         return(lista_barras)
 
     def agregar_restriccion(self, nodo, gdl, valor=0.0):
-        #Falta implementar	
-        return(0)
+        if nodo not in self.restricciones:
+            self.restricciones[nodo] = [[gdl, valor]]
+        else:
+            self.restricciones[nodo].append([gdl, valor])
 
     def agregar_fuerza(self, nodo, gdl, valor):
-        #Falta implementar	
-        return(0)
+        if nodo not in self.cargas:
+            self.cargas[nodo] = [[gdl, valor]]
+        else:
+            self.cargas[nodo].append([gdl, valor])
 
-    def ensamblar_sistema(self):
-        #Falta implementar	
-        return(0)
+#Agregar factor_peso_propio ¿(1+factor_peso_propio)?
+    def ensamblar_sistema(self, factor_peso_propio=0.0):
+        self.Ndof = self.Nnodos*self.Ndimensiones
+        self.k = np.zeros((self.Ndof, self.Ndof), dtype=np.double)
+        self.f = np.zeros((self.Ndof), dtype=np.double)
+        self.u = np.zeros((self.Ndof), dtype=np.double)
+        for i,b in enumerate(self.barras):
+            ke, fe = b.obtener_rigidez(self), b.obtener_vector_de_cargas(self)
+            n_i, n_j = b.obtener_conectividad()
 
+            #código video profe
+            if self.Ndimensiones == 2:
+                d = [2*n_i, 2*n_i+1, 2*n_j, 2*n_j+1]
+            else:
+                d = [3*n_i, 3*n_i+1, 3*n_i+2, 3*n_j, 3*n_j+1, 3*n_j+2]
+            for i in range(2*self.Ndimensiones):
+                p = d[i]
+                for j in range(2*self.Ndimensiones):
+                    q = d[j]
+                    self.k[p,q] += ke[i,j]
+                self.f[p] = fe[i]
+
+
+                
     def resolver_sistema(self):
-        #Falta implementar	
-        return(0)
-
+        """Implementar"""	
+        
+        return 0
+        
     def obtener_desplazamiento_nodal(self, n):
-        #Falta implementar	
-        return(0)
+        if self.Ndimensiones == 2:
+            dof = [2*n, 2*n+1]
+        else:
+            dof = [3*n, 3*n+1, 3*n+2]
+        return self.u[dof]
 
     def obtener_fuerzas(self):
-        #Falta implementar	
-        return(0)
+        fuerzas = np.zeros((len(self.barras)), dtype=np.double)
+        for i,b in enumerate(self.barras):
+            fuerzas[i] = b.obtener_fuerza(self)
+            self.fuerzas.append(fuerzas[i])
+        return(fuerzas)
 
     def obtener_factores_de_utilizacion(self, f):
         #Falta implementar	
@@ -78,5 +111,28 @@ class Reticulado(object):
         return(0)
 
     def __str__(self):
-     
-        return "Soy un reticulado :)"
+        s = 'nodos:\n'
+        for i in range(len(self.xyz)):
+            s += f'{i} : ({self.obtener_coordenada_nodal(i)})\n'
+        s += '\nbarras:\n'
+        for i in range(len(self.barras)):
+            s += f'{i} : {self.barras[i].ni,self.barras[i].nj}\n'
+        s += '\nrestricciones:\n'
+        for i in self.restricciones:
+            s += f'{i} : {self.restricciones[i]}\n'
+        s += '\ncargas:\n'
+        for i in self.cargas:
+            s += f'{i} : {self.cargas[i]}\n'
+        s += '\ndesplazamientos:\n'
+        t = self.u.reshape((-1,self.Ndimensiones))
+        for i in range(self.Nnodos):
+            if self.Ndimensiones == 2:
+                s += f'{i} : ({t[i,0]}, {t[i,1]}, {0.0})\n'
+            else:
+                s += f'{i} : ({t[i,0]}, {t[i,1]}, {t[i,2]})\n'
+        s += '\nfuerzas:\n'
+        for i in range(len(self.fuerzas)):
+            s += f'{i} : {self.fuerzas[i]}\n'
+        s += f'\nNdimensiones = {self.Ndimensiones}'
+        return(s)
+
